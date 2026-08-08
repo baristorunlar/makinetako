3f6e20f86bf5fd0da496c30973d2ada629e61eac
Barış Torunlar <32782265+baristorunlar@users.noreply.github.com>
2026-08-09 01:15:47 +0300

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+// Firebase Yapılandırma Bilgileri (Grukantako Projesi)
+window.firebaseConfig = {
+  apiKey: "AIzaSyAb_Q-TaPRGu0Nj59PH0AsYCS4IHgaWqOQ",
+  authDomain: "grukantako.firebaseapp.com",
+  projectId: "grukantako",
+  storageBucket: "grukantako.firebasestorage.app",
+  messagingSenderId: "273291150009",
+  appId: "1:273291150009:web:1b42f3caa4dc51264a5168",
+  measurementId: "G-1F71ZRPZKP"
+};
